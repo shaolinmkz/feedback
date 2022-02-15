3cf0385334372359b9028a6729e1e11e307f24f1
shaolinmkz <nwabuzor.obiora@gmail.com>
2022-02-15 18:43:40 +0100

    add feedback store

--- src/App.svelte
+++ src/App.svelte
@@ -2,35 +2,22 @@
 	import FeedbackForm from "./components/FeedbackForm.svelte";
 	import FeedbackList from "./components/FeedbackList.svelte";
 	import FeedbackStats from "./components/FeedbackStats.svelte";
+  import { FeedbackStore } from "./stores";
 
-	let feedbacks = [
-		{
-			id: 1,
-			rating: 10,
-			text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
-		},
-		{
-			id: 2,
-			rating: 9,
-			text: "Eum architecto quas ab veritatis magnam quaerat laboriosam assumenda cum animi blanditiis modi harum suscipit quis, beatae repellat reiciendis, id totam rerum.",
-		},
-		{
-			id: 3,
-			rating: 8,
-			text: "Veritatis magnam quaerat laboriosam assumenda cum animi blanditiis modi harum suscipit quis.",
-		},
-	];
+	$: feedbacks = $FeedbackStore
 
 	$: count = feedbacks.length;
 
 	$: average = feedbacks.reduce((acc, cur) => acc + cur.rating, 0) / count;
 
 	const handleDelete = ({ detail }) => {
-		feedbacks = feedbacks.filter(({ id }) => id !== detail);
+		const newFeedbacks = feedbacks.filter(({ id }) => id !== detail);
+    FeedbackStore.update(() => newFeedbacks);
 	};
 
   const handleAddReview = ({ detail }) => {
-    feedbacks = [{ ...detail, id: count + 1 }, ...feedbacks];
+    const newFeedbacks = [{ ...detail, id: count + 1 }, ...feedbacks];
+    FeedbackStore.update(() => newFeedbacks);
   };
 </script>
 
--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,11 +1,22 @@
 <script>
+  // import { onDestroy } from "svelte";
+
   import { scale, fade } from "svelte/transition";
   import FeedbackItem from "./FeedbackItem.svelte";
+  import { FeedbackStore } from "../stores"
 
-  export let feedbacks = [];
+  // let feedbacks = [];
+
+  // const unsubscribe = FeedbackStore.subscribe((data) => {
+  //   feedbacks = data;
+  // });
+
+  // onDestroy(() => {
+  //   unsubscribe();
+  // })
 </script>
 
-{#each feedbacks as feedback (feedback.id)}
+{#each $FeedbackStore as feedback (feedback.id)}
   <div in:scale out:fade>
     <FeedbackItem {feedback} on:delete-feedback />
   </div>
